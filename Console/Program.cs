a006235e10595abec063cb785a86293df432705e
raljoach <raljoach@yahoo.com>
2017-02-11 08:37:46 -0800

    rudimentary menu

--- Console/Program.cs
+++ Console/Program.cs
@@ -9,8 +9,16 @@ namespace HouseFlipper
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {            
+            Console.WriteLine("---------------------");
+            Console.WriteLine("HouseFlipper Console ");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Welcome {0}\\{1}!\n", Environment.UserDomainName, Environment.UserName);
+            Console.WriteLine("Choose an option from menu:");
+            Console.WriteLine("1) Load data");
+            Console.WriteLine("2) Query flips");
+            Console.WriteLine("3) Query active");
+            Console.WriteLine("4) Clear database");
             Console.Read();
         }
     }
