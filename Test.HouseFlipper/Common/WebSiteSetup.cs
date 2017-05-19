c71c641f1eeaa2ee3602536040cd84c48a33756a
raljoach <raljoach@yahoo.com>
2017-05-19 13:58:23 -0700

    first UI test

---
+++ Test.HouseFlipper/Common/WebSiteSetup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.HouseFlipper
+{
+    public class WebSiteSetup
+    {
+        public static void NewApplication()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test.HouseFlipper/SearchViewTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace Test.HouseFlipper
+{
+    [TestClass]
+    public class SearchViewTest
+    {
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            WebSiteSetup.NewApplication();
+        }        
+
+        [TestMethod]
+        public void Load()
+        {
+            var siteUrl = Settings.Get(Setting.SiteUrl);
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(siteUrl);
+            string title = string.Empty;
+            Assert.AreEqual(title, driver.Title);
+            var input = string.Empty;
+            driver.FindElement(By.Id(input))
+        }
+
+        
+    }
+}
