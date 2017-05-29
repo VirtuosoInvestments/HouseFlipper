524dceba258d924b0b4d1862a11d1620d53a5c5b
raljoach <raljoach@yahoo.com>
2017-05-29 16:02:15 -0700

    first stab at implementing CssSearch

--- Tests/WebSite/UI/CssSearch.cs
+++ Tests/WebSite/UI/CssSearch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,33 @@ namespace Test.HouseFlipper.WebSite
 {
     public class CssSearch
     {
-        public string TagName { get; internal set; }
-        public string Text { get; internal set; }
+        private StringDictionary attributes = new StringDictionary();
+        public string TagName { get; set; }
+        public string Text { get; set; }
+        public void Add(string attribute, string value)
+        {
+            attributes.Add(attribute, value);
+        }
+
+        public override string ToString()
+        {
+            var result = string.Format("{0}[", TagName);
+            bool isFirst = true;
+            if (!string.IsNullOrEmpty(Text)) { attributes.Add("text", Text); }
+            foreach(var a in attributes.Keys)
+            {
+                if(isFirst)
+                {
+                    isFirst = false;
+                }
+                else
+                {
+                    result += ",";
+                }
+                result += string.Format("{0}='{1}'", a, attributes[a]);
+            }
+            result += "]";
+            return result;
+        }
     }
 }
