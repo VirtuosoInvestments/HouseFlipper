e9411d5e3f55fccc5aaf49ea9a0f75b0f00e6431
raljoach <raljoach@yahoo.com>
2017-06-15 11:49:19 -0700

    new laptop, VS2017 tried to build with

--- Utility/Commands/SetupTestSiteCommand.cs
+++ Utility/Commands/SetupTestSiteCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,12 @@ namespace HouseFlipper.Utility
     {
         private static int port = 8020;
         private static string siteName = "HouseFlipperTest";
-        private static string siteDir = @"C:\Users\ralph.joachim\Documents\Visual Studio 2015\Projects\HouseFlipper\WebSite";
+        private static string siteDir;
+        public SetupTestSiteCommand()
+        {
+            //siteDir = @"C:\Users\ralph.joachim\Documents\Visual Studio 2015\Projects\HouseFlipper\WebSite";
+            siteDir = ConfigurationManager.AppSettings["SiteDir"];
+        }
         public override void Execute(string[] args)
         {
             base.Execute(port.ToString(),siteName,siteDir);
