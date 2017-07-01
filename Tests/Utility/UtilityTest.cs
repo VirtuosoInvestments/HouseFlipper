08aca0779b88e697978cb3d7bcfa8c16b4d8ff37
raljoach <raljoach@yahoo.com>
2017-06-30 17:55:31 -0700

    added ImportParallel test, but not implemented

--- Tests/Utility/UtilityTest.cs
+++ Tests/Utility/UtilityTest.cs
@@ -12,10 +12,19 @@ namespace Utility
         [Category("Incomplete")]
         public void Import()
         {
+            // TODO: Delete DB
             var exe = @"C:\Users\ralph.joachim\Documents\Visual Studio 2015\Projects\HouseFlipper\Utility\bin\Debug\util.exe";
             var args = @"-import ""E:\DocuSign\Backup\Laptop\My Documents\Visual Studio 2015\Projects\HouseFlipper\WebSite\Data\Listings""";
 
             var p = Process.Start(exe, args);
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [Category("NotImplemented")]
+        public void ImportParallel()
+        {
+            // TODO: Delete DB
             throw new NotImplementedException();
         }
     }
