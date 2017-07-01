a3a15ed8d96c80251dc2fe6fb74b5e60db616987
raljoach <raljoach@yahoo.com>
2017-06-30 21:27:12 -0700

    UtilityTest.Import: drop database, change db name to TestMLS and restore back MLS when done in util.exe.config

--- Tests/Utility/UtilityTest.cs
+++ Tests/Utility/UtilityTest.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using HouseFlipper.DataAccess.DB;
 using System.Linq;
 using System.Data.Entity;
+using System.IO;
 
 namespace Utility
 {
@@ -11,38 +12,59 @@ namespace Utility
     public class UtilityTest
     {
         [Test]
-        [Category("Incomplete")]
         public void Import()
         {
-            // TODO: Delete DB: either drop tables, or use sqlexec.cmd to delete MLS database or set app.config to point to a TestMls DB that can be dropped
-            /*using (var context = new MlsContext())
-            {
-                context.Database.Delete();
-                context.SaveChanges();
-            }*/
-
             Database.Delete("MlsContext");
 
             var exe = @"C:\Users\ralph.joachim\Documents\Visual Studio 2015\Projects\HouseFlipper\Utility\bin\Debug\util.exe";
-            var args = @"-import ""E:\DocuSign\Backup\Laptop\My Documents\Visual Studio 2015\Projects\HouseFlipper\WebSite\Data\Listings""";
-
-            var p = Process.Start(exe, args);
-            p.WaitForExit();
-
-            using (var context = new MlsContext())
+            var appconfig = exe + ".config";
+            var content = string.Empty;
+            using (var sr = new StreamReader(appconfig))
             {
-                var count = (from i in context.Listings
-                 select i).Count();
-                Assert.IsTrue(count > 0);
-                Assert.AreEqual(26587,count);
+                content = sr.ReadToEnd();
+            }
+            content = content.Replace("Initial Catalog=MLS", "Initial Catalog=TestMLS");
+            using (var sw = new StreamWriter(appconfig))
+            {
+                sw.Write(content);
+                sw.Flush();
+            }
 
-                var count2 = (from i in context.Properties
-                             select i).Count();
-                Assert.IsTrue(count2 > 0);
+            try
+            {
+                var args = @"-import ""E:\DocuSign\Backup\Laptop\My Documents\Visual Studio 2015\Projects\HouseFlipper\WebSite\Data\Listings""";
 
-                var count3 = (from i in context.PropertyListings
-                              select i).Count();
-                Assert.IsTrue(count3 > 0);
+                var p = Process.Start(exe, args);
+                p.WaitForExit((int)TimeSpan.FromMinutes(5).TotalMilliseconds);
+
+                using (var context = new MlsContext())
+                {
+                    var count = (from i in context.Listings
+                                 select i).Count();
+                    Assert.IsTrue(count > 0);
+                    Assert.AreEqual(26587, count);
+
+                    var count2 = (from i in context.Properties
+                                  select i).Count();
+                    Assert.IsTrue(count2 > 0);
+
+                    var count3 = (from i in context.PropertyListings
+                                  select i).Count();
+                    Assert.IsTrue(count3 > 0);
+                }
+            }
+            finally
+            {
+                using (var sr = new StreamReader(appconfig))
+                {
+                    content = sr.ReadToEnd();
+                }
+                content = content.Replace("Initial Catalog=TestMLS", "Initial Catalog=MLS");
+                using (var sw = new StreamWriter(appconfig))
+                {
+                    sw.Write(content);
+                    sw.Flush();
+                }
             }
         }
 
