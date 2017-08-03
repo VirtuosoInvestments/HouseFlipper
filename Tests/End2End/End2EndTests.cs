904194c6780194d0ebd165eabcdb6dddd19959d8
raljoach <raljoach@yahoo.com>
2017-08-03 16:57:47 -0700

    introduced E2E test project

---
+++ Tests/End2End/End2EndTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.HouseFlipper.Framework;
+
+namespace Test.HouseFlipper.End2End
+{
+    [TestFixture]
+    public class End2EndTests
+    {
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            LoadDb();
+        }
+
+        private void LoadDb()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void ListingsView()
+        {
+            var dbCount = new MlsDatabase().Listings.Count; // uses app.config to know which database to connect to
+            var listingsView = new ListingsView();  // uses app.config to know which site url to use
+            var results = listingsView.Results;
+            Assert.AreEqual(dbCount, results.Count);
+        }
+    }
+}
