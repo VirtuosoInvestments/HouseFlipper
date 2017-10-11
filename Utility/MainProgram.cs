7dc9b324b324abba260a4b6405693c4e0be53515
raljoach <raljoach@yahoo.com>
2017-10-11 05:55:20 -0700

    missing files + .gitignore introduced

---
+++ Utility/MainProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseFlipper.Utility
+{
+    public class MainProgram
+    {
+        public static void Main(string[] args)
+        {
+            var version = GetVersion();
+            if (version == "v2")
+            {
+                Program.Execute(args);
+            }
+            else
+            {
+                throw new InvalidOperationException("Unsupported version: " + version);
+            }
+        }
+        private static string GetVersion()
+        {
+            return ConfigurationManager.AppSettings["Version"].ToLower();
+        }
+    }
+}
